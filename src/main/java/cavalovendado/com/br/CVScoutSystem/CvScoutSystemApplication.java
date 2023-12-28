bc4a66eb72983b2a636dd1b5da3f5ffda44782f9
Eduardo Tamagno <eduardo.sala.et@gmail.com>
2023-12-27 23:17:13 -0300

    Preparando o hambiente

---
+++ src/main/java/cavalovendado/com/br/CVScoutSystem/CvScoutSystemApplication.java
@@ -0,0 +1,13 @@
+package cavalovendado.com.br.CVScoutSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvScoutSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvScoutSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cavalovendado/com/br/CVScoutSystem/CvScoutSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cavalovendado.com.br.CVScoutSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvScoutSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
